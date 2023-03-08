2752e3aa061ffa0292ddb392856421a67f5a0460
Lelouch <erasyl.kelman@gmail.com>
2023-03-09 03:22:35 +0600

    Правки

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -9,6 +9,7 @@
                 <div class="row">
                     <div
                     @foreach($students as $student)
+                        <div class="border">
                         <div class="col-lg-2 d-flex justify-content-center text-center mt-3">
                         <a href="{{route('student.show',$student->id)}}" class="link-dark text-decoration-none">
                             <div class=" card w-100">
@@ -19,6 +20,7 @@
                                 <h3 class="card-title">{{$student->name}}</h3>
                             </div>
                         </a>
+                        </div>
                         </div>
                     @endforeach
 
