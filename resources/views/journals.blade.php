81edce5d14c70f7ad24985fec9826905cc860ae3
Lelouch <erasyl.kelman@gmail.com>
2023-03-09 03:32:20 +0600

    Правки

--- resources/views/journals.blade.php
+++ resources/views/journals.blade.php
@@ -15,7 +15,7 @@
                                         <img src="{{asset('/storage/'.$journal->photos()->first()->path)}}"
                                              class="img-fluid p-3"
                                         style="max-height: 200px;min-height: 200px;
-                                            max-width: 150px;min-width: 150px"/>
+                                            min-width: 150px"/>
 
                                     <h3 class="card-title">{{$journal->name}}</h3>
                                 </div>
